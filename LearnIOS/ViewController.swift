a179d7dcd61f8e56a87a4452ec1407ed4f95cc18
Eduard Arley Gómez Muriel - Ceiba Software <eduard.gomez@users-macbook-air.local>
2018-06-21 10:56:06 -0500

    Initial Commit

---
+++ LearnIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnIOS
+//
+//  Created by Eduard Arley Gómez Muriel - Ceiba Software on 6/21/18.
+//  Copyright © 2018 eduardo76609. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
